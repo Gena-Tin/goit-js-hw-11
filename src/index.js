b5f30c6ad2456dad637e10d8b4090f5f50847743
GenaTin <111846926+Gena-Tin@users.noreply.github.com>
2022-11-16 23:13:54 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+
 import { fetchImages } from './js/fetchImages';
 import { createMarkup } from './js/markup';
 
