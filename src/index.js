da542ccc5ff4b08bdbc5cc6a6e040142ae6b01f9
GenaTin <111846926+Gena-Tin@users.noreply.github.com>
2022-11-17 22:36:31 +0200

    load more button

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './css/styles.css';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
+import InfiniteScroll from 'infinite-scroll';
 
 import { fetchImages } from './js/fetchImages';
 import { createMarkup } from './js/markup';
@@ -12,23 +12,85 @@ const ref = {
     inputField: document.querySelector("input"),
     loadMoreBtm: document.querySelector(".load-more"),
     imageList: document.querySelector(".gallery"),
+    endOfSearch: document.querySelector(".end-of-search")
     
 }
 
+let query = "";
+let page = 1;
+const perPage = 30;
+
+
 ref.loadMoreBtm.classList.add("is-hidden");
+ref.endOfSearch.classList.add("is-hidden");
+
+ref.loadMoreBtm.addEventListener("click", loadMoreHandler);
 ref.searchForm.addEventListener("submit", submitHandler);
+ 
+// Init InfiniteScroll ==========================
+// let elem = document.querySelector('.gallery');
+// let infScroll = new InfiniteScroll( elem, {
+//     path: ".load-more",
+//   append: '.gallery__link',
+//   history: false,
+//   status: '.page-load-status',
+// });
+// // =======
+// const statusBar = document.querySelector('.status-bar');
+
+// infScroll.on( 'load', function() {
+//   statusBar.textContent = `Loaded page: ${infScroll.pageIndex}`;
+// });
+//================================================
+
 
 function submitHandler(e){
     e.preventDefault();
+    window.scrollTo({ top: 0 });
+    query = ref.inputField.value.trim();
+    if(query===''){
+        Notify.warning('UPS. I can\'t find the void. Please try typing something');
+        return;
+    }
 
-    fetchImages(ref.inputField.value.trim())
-    .then(data => {
+    fetchImages(query, page, perPage)
+    .then(({data}) => {
+        ref.endOfSearch.classList.add("is-hidden");
+        console.log(data);
+
+        if(data.hits.length === 0){
+            Notify.warning('Sorry, there are no images matching your search query. Please try again.');
+        return;
+        }else{
+            Notify.success(`Hooray! We found ${data.totalHits} images.`);
+        };
+
+        if(data.totalHits > perPage){
+            ref.loadMoreBtm.classList.remove("is-hidden");
+        }
+
         ref.imageList.innerHTML = createMarkup(data.hits),
 
-        simpleLightBox = new SimpleLightbox('.gallery a').refresh()
+        simpleLightBox = new SimpleLightbox('.gallery a').refresh()      
+
     });
 }
 
+function loadMoreHandler(){
+    page += 1;
+    fetchImages(query, page, perPage)
+    .then(({data}) => {
+        const totalPages = Math.round(data.totalHits / perPage);
+        console.log(totalPages);
+        if(page >= totalPages){
+            ref.loadMoreBtm.classList.add("is-hidden");
+            ref.endOfSearch.classList.remove("is-hidden");
+            Notify.info('We\'re sorry, but you\'ve reached the end of search results.');
+        }
+        ref.imageList.insertAdjacentHTML('beforeend', createMarkup(data.hits));
+        simpleLightBox = new SimpleLightbox('.gallery a').refresh()
+    });
+}
 
 
 //Прокрутка страницы
@@ -43,8 +105,6 @@ function submitHandler(e){
 //   top: cardHeight * 2,
 //   behavior: "smooth",
 // });
-
-
 
 // Бесконечный скролл
 // Вместо кнопки «Load more» можно сделать бесконечную загрузку изображений при прокрутке страницы. Мы предоставлям тебе полную свободу действий в реализации, можешь использовать любые библиотеки.
--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,17 +1,17 @@
-const BASE_URL = "https://pixabay.com/api/?";
+import axios from 'axios';
+
+axios.defaults.baseURL = "https://pixabay.com/api/";
 
 const searchParams = new URLSearchParams({
     key:"31354744-e6340b12404bc1f4908fb1f36",
     image_type: "photo",
     orientation: "horizontal",
     safesearch: "true",
-    q:"",
   });
 
-export function fetchImages(searchImg){
-    return fetch(`${BASE_URL}${searchParams}${searchImg}`)
-    .then(response =>  {
-        if (!response.ok) {throw new Error(`Error ${response.statusText}`)}
-        return response.json();
-    })
+export async function fetchImages(searchImg, page, perPage){
+    const response = await axios.get(
+        `?${searchParams}&q=${searchImg}&page=${page}&per_page=${perPage}`,
+      );
+      return response;
 }
